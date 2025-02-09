4b917a95b41bd7c580e599912254ade174293ecd
Hitesh-145 <hiteshfulwariya145@gmail.com>
2025-02-09 14:04:33 +0000

    5

--- Basics/bs.js
+++ Basics/bs.js
@@ -1,1 +1,9 @@
-console.log("sdfghj")
+// const accId = 1234 
+// let accEmail =  "asdasdfrgh@gmail.come"
+// var accPassword = "12qwedfg"
+// accCity = "jodhpur"
+// let accState
+
+// console.table([accId, accEmail, accPassword, accCity, accState])
+
+console.log(typeof null) // object
