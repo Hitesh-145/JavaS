678c0a6aff193cf555cafad7265a2606f5b27bdb
Hitesh-145 <hiteshfulwariya145@gmail.com>
2025-02-09 16:34:48 +0000

    6-8

---
+++ Basics/conversion.js
@@ -0,0 +1,18 @@
+let score = "23as"
+
+console.log(typeof score)
+
+ valueInNum = Number(score);
+
+console.log(typeof valueInNum)
+console.log(valueInNum)
+
+// op
+console.log(2**3) // power 
+str1 = "fuck"
+str2 = " you"
+str3 = str1 +str2 
+console.log(str3)
+
+console.log("2" == 2)
+console.log("2" === 2)
