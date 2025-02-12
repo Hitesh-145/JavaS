fa49c1f50882351432099c55ad7ac5942aad00d5
Hitesh-145 <hiteshfulwariya145@gmail.com>
2025-02-12 19:04:31 +0000

    arr

---
+++ Basics02/array.js
@@ -0,0 +1,15 @@
+const arr = [1,2,4,5,6,7,7]
+const arr2 = [34,98,87,6,5,4]
+
+console.log(arr)
+
+// arr.push(arr2)
+
+console.log(arr)
+
+const arr3 = arr.concat(arr2)
+console.log(arr3)
+
+const arr4 = [...arr, ...arr2]
+console.log(arr4)
+
