22737175618a6b9fa225d2beaf7aead5c3fbd7d2
Hitesh-145 <hiteshfulwariya145@gmail.com>
2025-02-15 18:16:13 +0000

    iife

--- Basics02/function.js
+++ Basics02/function.js
@@ -19,3 +19,37 @@ function objFun(anyObj){
     console.log(`1 is ${anyObj.name} and 2 is ${anyObj.user}`);
 }
 objFun(obj)
+
+
+one(9)  // allowed
+function one(num){
+    return num+1;
+}
+
+//two(4) ---not allowed befor declration
+const two  =  function(num){
+    return num+2;
+}
+
+two(3)
+
+// arrow fun
+
+const htx = (x,y) => {
+  return x+y
+
+}
+
+const hrx = (x,y) => (x+y); // no need of return it's implisite
+
+// !!! imediataly invoked fun expression (IIFE)
+
+(
+    function three(){
+        console.log("sdfg")
+    }
+)(); // needed if more then two iife 
+
+(
+(name) => { console.log(`${name}, is there`)}
+)("hitesh")
